3ec55b93af0e927d2b0c2e3cca2ced0b4e897fb1
Edmond <yuenho092590@gmail.com>
2023-06-10 11:17:38 +0000

    fastapi

--- main.py
+++ main.py
@@ -2,8 +2,19 @@ from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
-#import sqlite3
+from pydantic import BaseModel
+import sqlite3
 import uvicorn
+
+class NameSearch(BaseModel):
+    category: str 
+    name: str
+    
+class NumSearch(BaseModel):
+    category: str
+    mininum: int
+    maximum: int
+    
 
 app = FastAPI(
     title='DB project'
@@ -12,8 +23,8 @@ app = FastAPI(
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-#conn = sqlite3.connect('data.db')
-#cursor = conn.cursor()
+conn = sqlite3.connect('data.db')
+cursor = conn.cursor()
 
 @app.get('/', response_class=HTMLResponse)
 async def main(request: Request):
@@ -21,8 +32,35 @@ async def main(request: Request):
 
 @app.get('/{category}', response_class=HTMLResponse)
 async def htmlQuery(category: str, request: Request):
-    print(category)
     return templates.TemplateResponse(f"{category} page.html",{'request':request})
+
+@app.post('/data/search_by_num')
+async def numQuery(item: NumSearch):
+    pass
+
+@app.post('/data/search_by_name')
+async def nameQuery(item: NameSearch):
+    pass
+
+
+def executeQuery(stmt: str):
+    try:
+        new_rows = []
+        cursor.execute(stmt)
+        rows = cursor.fetchall()
+        
+        for r in rows:
+            new_rows.append(list(r))
+            
+        
+    except Exception as e:
+        
+        return {'status': -1, 'details': e}
+    
+    
+    
+    return {'status': 200, 'details': new_rows}
+    
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8080)
