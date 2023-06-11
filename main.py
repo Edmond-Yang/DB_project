5b7baafe08424d7b3afab4ad804d9cd7d78ae2be
Edmond Yang <yuenho092590@gmail.com>
2023-06-11 10:05:24 +0000

    finish sql ver. 1

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
+from sql import *
 
 import uvicorn
 
@@ -26,7 +27,6 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
-
 @app.get('/', response_class=HTMLResponse)
 async def main(request: Request):
     return templates.TemplateResponse("home page.html",{'request':request})
@@ -44,8 +44,29 @@ async def numQuery(item: NumSearch):
     if item.mininum > item.maximum:
         return {'status': -1, 'details': '最小值不得大於最大值'}
     
+    try:
     
-    pass
+        if item.category == '任務':       
+            callQuest(['', ''], ['總經驗值', item.mininum, item.maximum])
+            
+        else:
+            
+            stmt = f'WHERE {item.objects} BETWEEN {item.mininum} AND {item.maximum}'
+            
+            if item.category == '裝備':
+                callGear([stmt*2])
+                        
+            elif item.category == 'NPC':
+                callNPC(stmt)
+                
+            elif item.category == '怪物':
+                callMonster(stmt)
+                
+    except Exception as e:
+        print(e)
+    
+    
+    
 
 @app.post('/data/search_by_name')
 async def nameQuery(item: NameSearch):
@@ -55,11 +76,51 @@ async def nameQuery(item: NameSearch):
     
     if item.name[0] == ' ':
         return {'status': -1, 'details': '名稱開頭不得為空'}
-    pass
-
-
-
     
-
+    
+    if item.category == '任務':
+        
+        subQuery = []
+        target = []
+        
+        if '任務' in item.objects:
+            stmt = f"WHERE 任務名稱 LIKE '%{item.name}%' OR 前置任務 LIKE '%{item.name}%'"
+            subQuery += [stmt*2]
+            target += [''*2]
+        else:
+            subQuery += [''*2]
+            target += [item.objects, item.name]
+        
+        try:
+            callQuest(subQuery, target)
+        except Exception as e:
+            print(e)
+        
+    else:
+        stmt = f"WHERE {item.objects} LIKE '%{item.name}%'"
+        
+        try:
+            if item.category == '裝備':
+                
+                if 'NPC' in item.objects:
+                    callGear([stmt, ''])
+                elif '怪物' in item.objects:
+                    callGear(['', stmt])
+                else:
+                    callGear([stmt*2])
+                    
+            elif item.category == 'NPC':
+                callNPC(stmt)
+                
+            elif item.category == '怪物':
+                callMonster(stmt)
+            
+            else:
+                pass
+                
+        except Exception as e:
+            print(e)
+        
+    
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8080)
+    uvicorn.run(app, host='0.0.0.0', port=8000)
--- sql.py
+++ sql.py
@@ -7,11 +7,11 @@ def swap(l: list, a: int, b: int):
     l[a], l[b] = l[b], l[a]
     return l  
 
-def callQuest(subStmt: str):
+def callQuest(subStmt: list, target: list):
     
     query = []
     
-    stmt = "SELECT * FROM 任務 JOIN NPC ON 接收NPC = NPC名稱 OR 回報NPC = NPC名稱 NATURAL JOIN 任務詳細"
+    stmt = "SELECT * FROM 任務 JOIN NPC ON 接收NPC = NPC名稱 OR 回報NPC = NPC名稱 NATURAL JOIN 任務詳細 " + subStmt[0]
     temp = executeQuery(stmt)
     
     for cnt in range(0, len(temp), 4):
@@ -26,24 +26,87 @@ def callQuest(subStmt: str):
         
         query.append(t)
     
-    stmt = "SELECT 任務名稱, 所在地圖, 經驗值 FROM 任務 NATURAL JOIN 任務詳細 NATURAL JOIN 怪物"
+    stmt = "SELECT 任務名稱, 所在地圖, 經驗值 FROM 任務 NATURAL JOIN 任務詳細 NATURAL JOIN 怪物 " + subStmt[1]
     temp = executeQuery(stmt)
     
-    print(query[0:5])
-    
-    # for cnt in range(0, len(temp), 2):
-    #     query[cnt//2] += temp[cnt] + temp[cnt+1]
+    for cnt in range(0, len(temp), 2):
+            query[cnt//2] += temp[cnt][1:] + temp[cnt+1][1:]
+            
+    for cnt in range(len(query)):
+        total = query[cnt][4] + query[cnt][8]*query[cnt][12]+query[cnt][10]*query[cnt][14]
+        query[cnt].append(total)
         
-    # for cnt in range(len(query)):
-    #     total = query[cnt][4] + query[cnt][8]*query[cnt][12]+query[cnt][10]*query[cnt][14]
-    #     query[cnt].append(total)
+    test = query[:]
+    query = []
+        
+    if target[0] == '怪物':
+        for t in test:
+            if target[1] in t[7] or target[1] in t[9]:
+                query.append(t)
+    elif target[0] == 'NPC':
+        for t in test:
+            if target[1] in t[1] or target[1] in t[5]:
+                query.append(t)
+    elif target[0] == '總經驗值':
+        for t in test:
+            if t[-1] >= target[1] and t[-1] <= target[2]:
+                query.append(t)
+        
+    query.sort(key=lambda x : x[-1], reverse=True)
     
+    print(query)
+
     
-    # query.sort(key=lambda x : x[-1], reverse=True)
+def callGear(subStmt: list):
     
-    # print(query)
+    stmt = "SELECT 裝備名稱, 來源NPC名稱, 所在地圖, 攻擊力, 玩家需求等級 FROM 裝備 JOIN NPC ON NPC名稱 = 來源NPC名稱 " + subStmt[0]
+    query = executeQuery(stmt)
+    
+    for q in query:
+        q.insert(1, 'NPC')
+    
+    stmt = "SELECT 裝備名稱, 來源怪物名稱, 所在地圖, 來源怪物名稱, 裝備.攻擊力, 玩家需求等級 FROM 裝備 JOIN 怪物 ON 來源怪物名稱 = 怪物名稱 " + subStmt[1]
+    temp = executeQuery(stmt)
+    
+    for t in temp:
+        t.insert(1, '怪物')
+        
+    query += temp
+    
+    choice = -2
+    for cnt, attr in enumerate(['裝備.攻擊力', '玩家需求等級']):
+        
+        if attr in subStmt[0] or attr in subStmt[1]:
+            choice = cnt+4
+            break
+    
+    query.sort(key=lambda x : x[choice], reverse=True)
+    
+    print(query)
     
 
+def callMonster(subStmt: str):
+    
+    stmt = "SELECT 怪物名稱, 所在地圖, 等級, 血量, 怪物.攻擊力, 防禦力, 經驗值, 裝備名稱 FROM 怪物 JOIN 裝備 ON 來源怪物名稱 = 怪物名稱 " + subStmt
+    temp = executeQuery(stmt)
+    
+    choice = -2
+    for cnt, attr in enumerate(['等級', '血量', '怪物.攻擊力', '防禦力', '經驗值']):
+        
+        if attr in subStmt:
+            choice = cnt+2
+            break
+        
+    temp.sort(key=lambda x : x[choice], reverse=True)
+    print(temp)
+    
+
+def callNPC(subStmt: str):
+    
+    stmt = "SELECT NPC名稱, 所在地圖, 裝備名稱 FROM NPC JOIN 裝備 ON 來源NPC名稱 = NPC名稱 " + subStmt
+    temp = executeQuery(stmt)
+    
+    print(temp)
 
 
 def executeQuery(stmt: str):
@@ -65,4 +128,7 @@ def executeQuery(stmt: str):
     return new_rows
 
 if __name__ == '__main__':
-    callQuest('')
+    callQuest(['',''], ['NPC', '美髮師娜塔麗'])
+    # callGear(['', ''])
+    # callNPC([''])
+    # callMonster([''])
